a3fc18781b0c0570f4694a8e76f3754a046bb3c2
Gonzalopaz41 <gonzalopazok@gmail.com>
2026-05-05 17:00:28 -0300

    payments documentation

--- src/payments/dto/create-payment.dto.ts
+++ src/payments/dto/create-payment.dto.ts
@@ -1,17 +1,21 @@
 import { IsDate, IsDateString, IsEnum, IsNotEmpty, IsNumber, IsPositive } from "class-validator";
 import { PaymentMethod } from "../entities/payment.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreatePaymentDto {
   
+  @ApiProperty({ description: 'Amount of the payment', example: 100.50 })
   @IsNotEmpty()
   @IsNumber()
   @IsPositive()
   amount!: number;
 
+  @ApiProperty({ description: 'Payment method', example: PaymentMethod.CASH })
   @IsNotEmpty()
   @IsEnum(PaymentMethod)
   method!: PaymentMethod;
 
+  @ApiProperty({ description: 'Date of the payment', example: '2023-10-15' })
   @IsNotEmpty()
   @IsDateString()
   paymentDate!: string;
